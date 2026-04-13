2b851727fa1659373dbdff20e0e6b13d8391678e
Daniel <danielara071@gmail.com>
2026-04-12 20:02:21 -0600

    like behavior implemented

--- src/features/reels/components/ReelsActionBar.tsx
+++ src/features/reels/components/ReelsActionBar.tsx
@@ -1,16 +1,20 @@
 import { Ellipsis, Heart, Share } from "lucide-react";
-import { useState } from "react";
 
 
 const crimsonColor = "#B5174B";
 
-const ReelsActionBar = () => {
+interface ReelsActionBarProps {
+  video_id: string;
+  liked: boolean;
+  onLike: (videoId: string) => void;
+}
 
-  const [liked, setLiked] = useState(false)
+const ReelsActionBar = ({video_id, liked, onLike} : ReelsActionBarProps) => {
+
 
   return (
     <div className="bg-brand-white h-12 w-60 rounded-4xl flex flex-row items-center justify-center gap-x-8 ">
-      <button className="cursor-pointer h-full w-7 flex items-center justify-center max-h-full" onClick={() => setLiked((prev) => !prev )}>
+      <button className="cursor-pointer h-full w-7 flex items-center justify-center max-h-full" onClick={() => onLike(video_id)}>
         <Heart color={crimsonColor}  fill={liked ? crimsonColor : "none"} />
       </button>
       <button className="cursor-pointer h-7 w-7 flex items-center justify-center">
