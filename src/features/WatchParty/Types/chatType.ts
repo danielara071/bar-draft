f8cf5578cb46e712bb19842fab691edc186a9301
FernandGS <a01029264@tec.mx>
2026-04-23 09:38:38 -0600

    separacion de tipos de mensajes en el chat

--- src/features/WatchParty/Types/chatType.ts
+++ src/features/WatchParty/Types/chatType.ts
@@ -1,6 +1,18 @@
-export type ChatMessage = {
-  message: string;
+type ChatMessage = 
+| {
+  type: "text";
   user_name?: string;
   avatar?: string;
   timestamp: string;
+  message: string;
+}
+| {
+  type: "sticker";
+  user_name?: string;
+  avatar?: string;
+  timestamp: string;
+  sticker_id: string;
+  sticker_url: string;
 };
+
+export type { ChatMessage };
