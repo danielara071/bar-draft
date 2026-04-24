2531fb1f1a2d9df35c29e8a770a9607af6c7db36
Daniel <danielara071@gmail.com>
2026-04-24 15:44:32 -0600

    inital for extended info view

---
+++ src/features/dashboard/components/ReelInfoCard.tsx
@@ -0,0 +1,17 @@
+
+
+
+const ReelInfoCard = () => {
+
+    return (
+        <div  className=" fixed top-0 left-0 w-full h-full bg-black/50 flex justify-center items-center z-40 ">
+            <div className="bg-brand-white z-50 rounded-2xl p-8">
+                
+            </div>
+        </div>
+    );
+}
+
+
+
+export default ReelInfoCard;
