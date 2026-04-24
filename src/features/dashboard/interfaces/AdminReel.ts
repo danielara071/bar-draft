5ac89de066b1317473287e34a644132b8d3a44e2
Daniel <danielara071@gmail.com>
2026-04-24 09:35:38 -0600

    admin reel structure

---
+++ src/features/dashboard/interfaces/AdminReel.ts
@@ -0,0 +1,13 @@
+
+
+export interface AdminReel {
+    id: string;
+    video_url: string;
+    thumbnail_url: string;
+    caption: string;
+    duration: number;
+    category: string;
+    order_index: number;
+    is_active: boolean;
+    created_at: Date;
+}
