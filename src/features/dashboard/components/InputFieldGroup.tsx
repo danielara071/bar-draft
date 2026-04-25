729d2de1ff757457214d771e0e8e8cdb6419433e
Daniel <danielara071@gmail.com>
2026-04-25 13:57:45 -0600

    shadcn components

---
+++ src/features/dashboard/components/InputFieldGroup.tsx
@@ -0,0 +1,36 @@
+import { Button } from "../../../shared/components/ui/shadcn/ui/button"
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+} from "../../../shared/components/ui/shadcn/ui/field"
+import { Input } from "../../../shared/components/ui/shadcn/ui/input"
+
+export function InputFieldgroup() {
+  return (
+    <FieldGroup>
+      <Field>
+        <FieldLabel htmlFor="fieldgroup-name">Name</FieldLabel>
+        <Input id="fieldgroup-name" placeholder="Jordan Lee" />
+      </Field>
+      <Field>
+        <FieldLabel htmlFor="fieldgroup-email">Email</FieldLabel>
+        <Input
+          id="fieldgroup-email"
+          type="email"
+          placeholder="name@example.com"
+        />
+        <FieldDescription>
+          We&apos;ll send updates to this address.
+        </FieldDescription>
+      </Field>
+      <Field orientation="horizontal">
+        <Button type="reset" variant="outline">
+          Reset
+        </Button>
+        <Button type="submit">Submit</Button>
+      </Field>
+    </FieldGroup>
+  )
+}
