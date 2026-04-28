d2320ab3e54bf5b8bd5e90b01938fb81066a6eb3
Daniel <danielara071@gmail.com>
2026-04-28 14:52:50 -0600

    add new reel button

---
+++ src/features/dashboard/components/AddNewReel.tsx
@@ -0,0 +1,20 @@
+import { Plus } from "lucide-react";
+
+interface AddNewReelProps {
+    onClick: () => void;
+}
+
+const AddNewReel = ({onClick} : AddNewReelProps) => {
+  return (
+    <div className="h-96 max-w-60 w-full rounded-2xl border border-brand-navy border-dashed relative cursor-pointer" onClick={onClick}>
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <Plus
+          className="h-9 w-9"
+          color="#0F2D52"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default AddNewReel;
