c4949589aee7fcc774235c4c0d0ff3d4c5be3d82
FernandGS <a01029264@tec.mx>
2026-04-29 14:09:50 -0600

    auth error card added to appear in the home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,20 @@
-import LoggedIn from '../features/home/LoggedIn';
-import LoggedOut from '../features/home/LoggedOut';
-import  useSession from "../shared/hooks/useSession"
-
+import LoggedIn from "../features/home/LoggedIn";
+import LoggedOut from "../features/home/LoggedOut";
+import AuthErrorCard from "../features/home/AuthErrorCard";
+import useAuthErrorNotice from "../shared/hooks/useAuthErrorNotice";
+import useSession from "../shared/hooks/useSession";
 
 function Home() {
-    const session = useSession();
-    return (
-        <>
-            {session ? <LoggedIn /> : <LoggedOut />}
-        </>
-    ) 
+  const { authMessage, isOpen, close } = useAuthErrorNotice();
+  const session = useSession();
+  return (
+    <>
+      {authMessage && isOpen && (
+        <AuthErrorCard message={authMessage} onClose={close} />
+      )}
+      {session ? <LoggedIn /> : <LoggedOut />}
+    </>
+  );
 }
 
 export default Home;
