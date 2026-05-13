21bdfc4851c9dc55bf9f3296a3404f35fdb575ee
DiegoCF8377 <a00835977@tec.mx>
2026-05-13 15:53:55 -0600

    corregir logica de boton perfil

--- src/features/perfil/PerfilUsuario.tsx
+++ src/features/perfil/PerfilUsuario.tsx
@@ -55,12 +55,14 @@ export default function PerfilUsuario({
         
         <div className="py-4 flex gap-14">
             <h2 className="text-white text-4xl font-semibold">@{username}</h2>
-            {/** <button
-                onClick={onLogoutFunc}
-                className="text-white bg-[#A50044] hover:bg-pink-700 px-4 py-2 rounded-full text-md"
-            >
-                {onLogoutText}
-            </button>*/}
+            {onLogoutText === "Cerrar sesión" ? null : (
+                <button
+                    onClick={onLogoutFunc}
+                    className="text-white bg-[#A50044] hover:bg-pink-700 px-4 py-2 rounded-full text-md"
+                >
+                    {onLogoutText}
+                </button>
+            )}
         </div>
 
         <div className="flex flex-row gap-1 mb-5">
