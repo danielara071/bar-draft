fdb01c1bb8c900f4e51c1eea930744a68ca153ab
Milan Guzman <andremilangr@hotmail.com>
2026-05-14 14:37:40 -0600

    First draft of AnimatePresence

--- src/shared/components/ChatbotWidget.tsx
+++ src/shared/components/ChatbotWidget.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { AnimatePresence, motion } from "motion/react"
 
 const logoURL = "https://vsywrimuzdnfyztreolz.supabase.co/storage/v1/object/sign/logo/barcelona_logo.png?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV85YjVhN2I1MC1iNThkLTRkMzEtOTJiZS1jMWRjNjdmZjY5MGYiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJsb2dvL2JhcmNlbG9uYV9sb2dvLnBuZyIsImlhdCI6MTc3ODc4NjIxMiwiZXhwIjoxODEwMzIyMjEyfQ.H2LaUn3HgO4lTLx2n8bpFjfHs3X2aXG8noWz9yDgRBQ";
 
@@ -8,7 +9,7 @@ const ChatbotWidget = () => {
   return (
     <div className="fixed bottom-6 right-6 z-50 flex flex-col items-end gap-3">
       {open && (
-        <div className="w-80 h-96 bg-white rounded-2xl shadow-xl border border-slate-200 flex flex-col overflow-hidden">
+        <div className="w-280 h-120 bg-white rounded-2xl shadow-xl border border-slate-200 flex flex-col overflow-hidden">
           <div
             className="p-4 flex items-center gap-2"
             style={{
@@ -23,6 +24,39 @@ const ChatbotWidget = () => {
           </div>
         </div>
       )}
+
+     
+      <AnimatePresence>
+        {open && (
+          <motion.button
+            onClick={() => setOpen(!open)}
+            aria-label="Abrir asistente Barça"
+            className="relative w-20 h-20 rounded-full shadow-lg hover:scale-110 active:scale-95 transition-transform duration-200"
+            style={{
+              background: `conic-gradient( #A50044 0deg 180deg, #004D98 180deg 360deg)`,
+              padding: "8px",
+            }}
+            whileHover={{ opacity: 1, x: 0}}
+            initial={{ opacity: 0, x: 50 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: 50 }}
+            transition={{ duration: 2 }}
+          >
+          Barca Assistent
+            <div className="w-full h-full rounded-full bg-white flex items-center justify-center">
+              <img
+                src={logoURL}
+                alt="FC Barcelona"
+                className="w-9 h-9 object-contain"
+              />
+            </div>
+
+          </motion.button>
+        )}
+
+        
+      </AnimatePresence>
+
 
       <button
         onClick={() => setOpen(!open)}
