7984136b57e58a67101b49f7e4c3fe1d0778ffcd
DiegoCF8377 <a00835977@tec.mx>
2026-05-15 10:01:25 -0600

    Latitud y longitud

--- src/features/adminMapaTrofeos/cardAgregarTrofeo.tsx
+++ src/features/adminMapaTrofeos/cardAgregarTrofeo.tsx
@@ -36,11 +36,28 @@ function CardAgregarTrofeo({
               </div>
               {/*Fin Columnas*/}
               <label className="text-sm text-brand-yellow font-sans">COORDENADAS</label>
-              <input
-                  type="text"
-                  placeholder="Trofeo Barcelona"
-                  className="w-full bg-white text-gray-500 rounded-full py-3 px-6 outline-none focus:ring-2 focus:ring-[#f4bd0e]"
-                  />
+              <div className="grid grid-cols-2 gap-8 mt-4 mb-4">
+                <div className="flex flex-col gap-2">
+                  <label className="text-sm text-brand-yellow font-sans ">
+                    Latitud
+                  </label>
+                  <input
+                    type="number"
+                    placeholder="100000"
+                    className="w-full bg-white text-gray-500 rounded-full py-3 px-6 outline-none focus:ring-2 focus:ring-[#f4bd0e]"
+                    />
+                </div>
+                <div className="flex flex-col gap-2">
+                  <label className="text-sm text-brand-yellow">
+                    Longitud
+                  </label>
+                  <input
+                    type="number"
+                    placeholder="100000"
+                    className="w-full bg-white text-gray-500 rounded-full py-3 px-6 outline-none focus:ring-2 focus:ring-[#f4bd0e]"
+                   />
+                </div>
+              </div>
               {/*botones*/}
               <div className="flex justify-between items-center mt-8">
                 <button className="bg-brand-yellow hover:bg-[#d9a90d] text-[#001d3d] font-bold py-3 px-10 rounded-full transition-colors duration-200"
